1de68e2de68de0d1612179bfe3dc55ab04e6cce0
Dan_Kerslager <dan.kerslager@tul.cz>
2024-04-26 21:11:35 +0200

    Verze pro vložení do code critic

---
+++ pour_final.py
@@ -0,0 +1,100 @@
+import heapq
+import sys
+
+
+class Node:
+    """
+    Třída reprezentující stavy lahvý po jednotlivých přelitích, tudíž i vrcholy grafu.
+    """
+    def __init__(self, bottles, state, cost, transfers):
+        self.bottles = bottles  # objemy nádob
+        self.state = state  # aktuální stav nádob
+        self.cost = cost  # celková doba přelití
+        self.transfers = transfers  # počet přelití
+
+    def __lt__(self, other):
+        # potřebujeme porovnávat uzly podle ceny pro heapq
+        return self.cost < other.cost
+
+
+def create_nodes(first_node):
+    """
+    Řídící metoda tvorby nodes, využívající principu průchodu do šířky.
+    """
+    queue = [first_node]
+    heapq.heapify(queue)
+    states = {tuple(first_node.state): [first_node.cost, 0]}
+    max_vol = first_node.bottles[-1]
+    costs = [float('inf')] * (max_vol+1)
+    transfers = [float('inf')] * (max_vol+1)
+    costs[max_vol] = 0
+    transfers[max_vol] = 0
+    nums = set(range(1, max_vol+1))
+    while queue:
+        node = heapq.heappop(queue)
+        nums -= set(node.state)
+        if len(set(nums)) == 0 and min(costs) < node.cost:
+            return costs, transfers
+        for i, state in enumerate(node.state):
+            if state != 0:
+                for j in range(len(node.state)):
+                    if i != j and node.state[j] != node.bottles[j]:
+                        new_node = pour(node, i, j)
+                        new_state = tuple(new_node.state)
+                        if new_state not in states or (new_node.cost, new_node.transfers) < tuple(states[new_state]):
+                            states[new_state] = [new_node.cost, new_node.transfers]
+                            heapq.heappush(queue, new_node)
+                            for volume in new_state:
+                                if new_node.cost < costs[volume]:
+                                    costs[volume] = new_node.cost
+                                    transfers[volume] = new_node.transfers
+                                elif new_node.cost == costs[volume]:
+                                    transfers[volume] = min([transfers[volume], new_node.transfers])
+    return costs, transfers
+
+
+def pour(node, from_index, to_index):
+    """
+    Metoda provadějící přelití z lahve do lahve a tudíž i vytvoření new_node.
+    """
+    pour_amount = min([node.state[from_index], (node.bottles[to_index] - node.state[to_index])])
+    new_cost = node.cost + pour_amount
+    new_transfers = node.transfers + 1
+    new_state = list(node.state)
+    new_state[from_index] -= pour_amount
+    new_state[to_index] += pour_amount
+    return Node(node.bottles, new_state, new_cost, new_transfers)
+
+
+def print_output(costs, transfers):
+    """
+    Vypíše vyžadovaný output ze získaných nejkradších cesty k jednotlivím objemům.
+    """
+    for i in range(1, len(costs)):
+        print(i, end=" ")
+        if costs[i] < float('inf'):
+            print(costs[i], transfers[i])
+        else:
+            print()
+
+
+def main():
+    """
+    Hlavní řídící funkce programu a funkce načítající vstupy.
+    """
+    max_volumes = []
+    amount = 0
+    for i, line in enumerate(sys.stdin):
+        if i == 0:
+            amount = int(line.strip().replace("\n", ""))
+        if i != 0 and i <= amount:
+            max_volumes.append(int(line.strip().replace("\n", "")))
+    max_volumes.sort()
+    start_volume = ([0] * (len(max_volumes) - 1)) + [max_volumes[-1]]
+    first_node = Node(max_volumes, start_volume, 0, 0)
+    costs, transfers = create_nodes(first_node)
+    print_output(costs, transfers)
+
+
+if __name__ == "__main__":
+    main()
